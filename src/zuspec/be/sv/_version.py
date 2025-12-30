dc5fccdfd5cb18d1bfb3c0ad2c8d9a4d34f03474
Matthew Ballance <matt.ballance@gmail.com>
2025-12-30 09:13:15 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/zuspec/be/sv/_version.py
+++ src/zuspec/be/sv/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev6+ga975a18e0.d20251220'
-__version_tuple__ = version_tuple = (0, 1, 'dev6', 'ga975a18e0.d20251220')
+__version__ = version = '0.1.dev9+gfcba01a7a.d20251225'
+__version_tuple__ = version_tuple = (0, 1, 'dev9', 'gfcba01a7a.d20251225')
 
-__commit_id__ = commit_id = 'ga975a18e0'
+__commit_id__ = commit_id = 'gfcba01a7a'
