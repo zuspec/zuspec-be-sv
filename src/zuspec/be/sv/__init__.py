b58b8872dc04743ca69eb95b9803170c92b7377a
Matthew Ballance <matt.ballance@gmail.com>
2026-04-14 23:08:30 +0000

    Checkpoint
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/zuspec/be/sv/__init__.py
+++ src/zuspec/be/sv/__init__.py
@@ -1,3 +1,3 @@
-from .generator import SVGenerator
+from .generator import SVGenerator, V2005FSMEmitter, generate_fsm
 
-__all__ = ["SVGenerator"]
+__all__ = ["SVGenerator", "V2005FSMEmitter", "generate_fsm"]
